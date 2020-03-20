412eced71f1740b3760380a217137c6151f59fd3
TheSirDingle <60834741+TheSirDingle@users.noreply.github.com>
2020-03-19 21:08:56 -0500

    Update while statements.py

--- Chapter 2 if,while,for, and importing/if staments, and while loops/while statements.py
+++ Chapter 2 if,while,for, and importing/if staments, and while loops/while statements.py
@@ -16,21 +16,25 @@ while name != 'your name':
 print('Thank you!')
 
 while True:
-    print('Please type your name.')
+    print('Please type your name again.')
     name = input()
     if name == 'your name':
         #if the statement is true it breaks out of the while statement to print statement
         break
-print('Thank you!')
+print('Thank you again!')
+
 
 while True:
  print('Who are you?')
  name = input()
- if name != 'Joe' or 'joe':
-     #Loop doesn't continue if name isn't joe. Continue statement jumps back to start of while loop and reevaluates the loop
+ if name != 'Joe':
+     #Loop doesn't continue if name isn't joe. break statement jumps back to start of while loop and reevaluates the loop
      continue
  print('Hello, Joe. What is the password? (It is a fish.)')
  password = input()
  if password == 'swordfish':
      break
+ else:
+     print('Wrong Password, please enter again')
+     continue
 print('Access granted.') 
